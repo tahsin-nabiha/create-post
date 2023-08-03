028e7b6278e499deb7fc5825d2a08ee324f15b6a
tahsin-nabiha <139380119+tahsin-nabiha@users.noreply.github.com>
2023-08-03 02:34:57 +0200

    Update

--- Facebook/App/function.js
+++ Facebook/App/function.js
@@ -1,6 +1,6 @@
 /**
  * Function for Validation Alert
- */
+*/
 
 const creatAlert = (msg, type='danger') => {
     return `
@@ -9,3 +9,64 @@ const creatAlert = (msg, type='danger') => {
             </p>
     `;
 }
+
+
+/**
+ * Function for Set LS Data
+*/
+
+const setLsData = (key, data) => {
+    localStorage.setItem(key, JSON.stringify(data));
+}
+
+
+
+/**
+ * Function for Get LS Data
+*/
+
+const getLsData = (key) => {   
+
+    if (localStorage.getItem(key)) {
+        return JSON.parse(localStorage.getItem(key));
+    }
+    return [];
+}
+
+
+
+
+/**
+ * Function for Post Time Ago
+*/
+
+function timeAgo(date) {
+    const secondsAgo = Math.floor((Date.now() - date) / 1000);
+  
+    const intervals = [
+      { name: "year", seconds: 31536000 },
+      { name: "month", seconds: 2592000 },
+      { name: "week", seconds: 604800 },
+      { name: "day", seconds: 86400 },
+      { name: "hour", seconds: 3600 },
+      { name: "minute", seconds: 60 },
+      { name: "second", seconds: 1 },
+    ];
+  
+    let timeAgoString = "Just now";
+  
+    for (let i = 0; i < intervals.length; i++) {
+      const interval = intervals[i];
+      const intervalCount = Math.floor(secondsAgo / interval.seconds);
+      if (intervalCount >= 1) {
+        timeAgoString = `${intervalCount} ${interval.name}${intervalCount === 1 ? "" : "s"} ago`;
+        break; // Break the loop once the correct interval is found
+      }
+    }
+  
+    return timeAgoString;
+  }
+  
+  
+  
+  
